1b63a131026ab0a61e25d624309333b6af7c79f1
sergpank <sergpank@gmail.com>
2018-12-16 18:54:04 +0200

    - added logger to BoardDao

--- src/main/java/md/onemap/MAIN.java
+++ src/main/java/md/onemap/MAIN.java
@@ -3,12 +3,11 @@ package md.onemap;
 import md.onemap.forum.dao.BoardDao;
 import md.onemap.forum.entity.Board;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MAIN
 {
-  public static void main(String[] args) throws SQLException
+  public static void main(String[] args)
   {
     List<Board> boards = new BoardDao().loadAll();
     System.out.println(boards);
--- src/main/java/md/onemap/forum/dao/BoardDao.java
+++ src/main/java/md/onemap/forum/dao/BoardDao.java
@@ -3,6 +3,9 @@ package md.onemap.forum.dao;
 import md.onemap.forum.entity.Board;
 import md.onemap.util.DbUtil;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,6 +15,8 @@ import java.util.List;
 
 public class BoardDao
 {
+  private static Logger log = LogManager.getLogger(BoardDao.class);
+
   public List<Board> loadAll()
   {
     List<Board> boards = new ArrayList<>();
@@ -31,7 +36,7 @@ public class BoardDao
     }
     catch (SQLException e)
     {
-      e.printStackTrace();
+      log.error("Unable to load boards.", e);
     }
 
     return boards;
